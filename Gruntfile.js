cc8b5beb874476dd4c7523d8cb67e63ece82e3dc
Corinne Krych <corinnekrych@gmail.com>
2017-04-19 14:38:16 +0200

    :recycle: replace turbo by mocha

--- Gruntfile.js
+++ Gruntfile.js
@@ -77,14 +77,14 @@ module.exports = function(grunt) {
           stdout: true,
           stderr: true
         },
-        command: 'env NODE_PATH=. ./node_modules/.bin/mocha -A -u exports --recursive test/unit/'
+        command: 'env NODE_PATH=. ./node_modules/.bin/mocha -A -u exports --recursive test/unit/test*.js'
       },
       accept: {
         options: {
           stdout: true,
           stderr: true
         },
-        command: 'env NODE_PATH=. ./node_modules/.bin/mocha -A -u exports --recursive test/server.js test/accept/'
+        command: 'env NODE_PATH=. ./node_modules/.bin/mocha -A -u exports --recursive test/accept/'
       },
       coverage_unit: {
         options: {
@@ -93,9 +93,7 @@ module.exports = function(grunt) {
         },
         command: [
           'rm -rf coverage cov-unit',
-          'env NODE_PATH=. ./node_modules/.bin/istanbul cover --dir cov-unit ./node_modules/.bin/turbo -- test/unit',
-          './node_modules/.bin/istanbul report',
-          'echo "See html coverage at: `pwd`/coverage/lcov-report/index.html"'
+          'env NODE_PATH=. ./node_modules/.bin/istanbul cover  --dir cov-unit ./node_modules/.bin/_mocha -- -u exports -R spec ./test/unit/*.js'
         ].join('&&')
       },
       coverage_accept: {
@@ -105,8 +103,7 @@ module.exports = function(grunt) {
         },
         command: [
           'rm -rf coverage cov-accept',
-          'env NODE_PATH=. ./node_modules/.bin/istanbul cover --dir cov-accept ./node_modules/.bin/turbo -- --setUp=test/accept/server.js --tearDown=test/accept/server.js test/accept',
-          './node_modules/.bin/istanbul report',
+          'env NODE_PATH=. ./node_modules/.bin/istanbul cover  --dir cov-unit ./node_modules/.bin/_mocha -- -u exports -R spec ./test/accept/*.js',
           'echo "See html coverage at: `pwd`/coverage/lcov-report/index.html"'
         ].join('&&')
       }
@@ -146,8 +143,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Testing tasks
-  grunt.registerTask('test', ['jshint', 'shell:unit', 'shell:accept']);
-  grunt.registerTask('unit', ['jshint', 'shell:unit']);
+  grunt.registerTask('test', ['shell:unit', 'shell:accept']);
+  grunt.registerTask('unit', ['shell:unit']);
   grunt.registerTask('accept', ['env:local', 'shell:accept']);
 
   // Coverate tasks
--- test/accept/test-hello.js
+++ test/accept/test-hello.js
@@ -2,8 +2,8 @@
 var request = require("request");
 var util = require('util');
 var assert = require('assert');
-
-var baseUrl = "http://127.0.0.1:8052/hello";
+var app = require('../server.js');
+var baseUrl = "http://127.0.0.1:8001";
 
 /*
  * A very simple acceptance test. This test requires the server to be running (this is setup via the 'grunt accept' task)
@@ -12,6 +12,7 @@ exports.it_should_test_hello_in_server_GET = function(finish){
   request(baseUrl + '?hello=test-get', function(err, response, body){
     assert.ok(!err, 'Unexpected error: ', util.inspect(err));
     assert.equal(response.statusCode, 200, 'Unexpected statusCode: ', response.statusCode + ' - ' + util.inspect(body));
+    //console.log(':::::::::::::::'+response.statusCode + ' - ' + util.inspect(body));
     var data = JSON.parse(body);
     assert.equal(data.msg, 'Hello test-get');
     finish();
@@ -19,7 +20,7 @@ exports.it_should_test_hello_in_server_GET = function(finish){
 };
 
 exports.it_should_test_hello_in_server_POST = function(finish){
-  request.post({url: baseUrl, json:{'hello':'test-post'}}, function(err, response, body){
+  request.post({url: baseUrl, json:{'read':'test-post'}}, function(err, response, body){
     console.log('Body  == ', body);
     assert.ok(!err, 'Unexpected error: ', util.inspect(err));
     assert.equal(response.statusCode, 200, 'Unexpected statusCode: ', response.statusCode + ' - ' + util.inspect(body));
